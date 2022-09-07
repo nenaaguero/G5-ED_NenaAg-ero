1ee21f69ef378e927c43752d1b0ed9d94bd86888
Nena Aguero Garcia <elena.aguero@cetys.edu.mx>
2022-09-06 18:31:35 -0700

    Primeros pasos con C·

---
+++ Primeros pasos con C#/Hola_mundo/Hola_mundo/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hola mundo")
+
+        Console.Write("Hola")
+        Console.Write(" ")
+        Console.Write("mundo")
+
+        Console.Read()
+    End Sub
+
+End Module
---
+++ Primeros pasos con C#/OperacionesBasicas/OperacionesBasicas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float w, x, y, z;
+
+            w = 25 + 9;
+            x =  25 - 9;
+            y = 25 * 9;
+            z = 25 / 9;
+
+            Console.WriteLine(w);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            Console.Read();
+        }
+    }
+}
